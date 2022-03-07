fde6769f2c8509421af57cb478162158705f04a7
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-03-07 23:03:09 +0900

    백준1000번

---
+++ src/algo1/baekjoon1.java
@@ -0,0 +1,26 @@
+package algo1;
+
+import java.util.Scanner;
+
+public class baekjoon1 {
+
+
+    public static void main(String[] args) {
+//        문제
+//        두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//        입력
+//        첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//        출력
+//        첫째 줄에 A+B를 출력한다.
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        print(a,b);
+    }
+
+
+    public static void print(int a, int b) {
+        System.out.println("출력 = " + a+b);
+    }
+}
