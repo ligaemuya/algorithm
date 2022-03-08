49fa9f5c4df09364366945170a0bbccaf51dcb44
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-03-08 23:05:52 +0900

    출력문제들

--- src/algo1/Baekjoon1.java
+++ src/algo1/Baekjoon1.java
@@ -3,11 +3,14 @@ package algo1;
 import java.util.Scanner;
 
 public class Baekjoon1 {
-
-
     public static void main(String[] args) {
-//        문제
-//        두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+        printDog();
+    }
+
+
+    public static void printAB() {
+        //        문제
+//        두 정수 A와 `B`를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 //        입력
 //        첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
 //        출력
@@ -16,11 +19,52 @@ public class Baekjoon1 {
         int a, b;
         a = sc.nextInt();
         b = sc.nextInt();
-        print(a,b);
+        System.out.println(a+b);
+    }
+
+    public static void printArmy() {
+//        문제
+//        ACM-ICPC 인터넷 예선, Regional, 그리고 World Finals까지 이미 2회씩 진출해버린 kriii는 미련을 버리지 못하고 왠지 모르게 올해에도 파주 World Finals 준비 캠프에 참여했다.
+//        대회를 뜰 줄 모르는 지박령 kriii를 위해서 격려의 문구를 출력해주자.
+//
+//        입력
+//        본 문제는 입력이 없다.
+//
+//        출력
+//        두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+
+    public static void printHelloworld() {
+//        문제
+//        Hello World!를 출력하시오.
+//
+//        입력
+//        없음
+//
+//        출력
+//        Hello World!를 출력하시오.
+        System.out.println("Hello World!");
+    }
+
+    public static void printCat(){
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+
+    public static void printDog(){
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
     }
 
 
-    public static void print(int a, int b) {
-        System.out.println("출력 = " + a+b);
-    }
+
 }
--- src/test.java
+++ src/test.java
@@ -1,3 +1,5 @@
+import algo1.Baekjoon1;
+
 public class test {
 
     public static void main(String[] args) {
