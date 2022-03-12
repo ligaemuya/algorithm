517201d65ecba1832eb473547e5be44ef939b8a2
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-03-12 22:31:57 +0900

    시험성적

--- src/algo1/Baekjoon2.java
+++ src/algo1/Baekjoon2.java
@@ -33,4 +33,32 @@ public class Baekjoon2 {
             System.out.println("==");
         }
     }
+
+
+    public static void test2() {
+//        문제
+//        시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+//        입력
+//        첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+//        출력
+//        시험 성적을 출력한다.
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        if(a>= 90){
+            System.out.println("A");
+        } else if (a >= 80){
+            System.out.println("B");
+        } else if (a>= 70){
+            System.out.println("C");
+        } else if (a>= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+
+
+
+    }
 }
